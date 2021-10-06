3a61bcb5065b89ce9547ff5222f01c491c5fd095
Antonio <ant.ruggiero16@gmail.com>
2021-10-06 17:54:16 +0200

    first upload x rot 70 vanguard

---
+++ x-rot-70-vanguard/__settings__.js
@@ -0,0 +1,21 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1245131.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 56842619, 56842610, 56842627, 56842615, 56842974 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+];
