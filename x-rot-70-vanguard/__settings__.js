e06f26ab1045378835266f3e031be628c5ea1080
Antonio Ruggiero <antonio.ruggiero@uqido.com>
2021-10-13 15:14:12 +0200

    updated Rot X model, added bottombar ui

--- x-rot-70-vanguard/__settings__.js
+++ x-rot-70-vanguard/__settings__.js
@@ -8,7 +8,7 @@ CONTEXT_OPTIONS = {
     'preferWebGl2': true,
     'powerPreference': "default"
 };
-SCRIPTS = [ 56842619, 56842610, 56842627, 56842615, 56842974 ];
+SCRIPTS = [ 56842619, 56842610, 56842627, 56842615, 56842974, 56859550, 56859587, 56924531 ];
 CONFIG_FILENAME = "config.json";
 INPUT_SETTINGS = {
     useKeyboard: true,
@@ -18,4 +18,5 @@ INPUT_SETTINGS = {
 };
 pc.script.legacy = false;
 PRELOAD_MODULES = [
+    {'moduleName' : 'BASIS', 'glueUrl' : 'files/assets/57449606/1/basis.wasm.js', 'wasmUrl' : 'files/assets/57449607/1/basis.wasm.wasm', 'fallbackUrl' : 'files/assets/57449605/1/basis.js', 'preload' : false},
 ];
